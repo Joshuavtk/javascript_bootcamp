6d788ea84a74ee1c1afbd1a291ca4a6297043ae4
Joshua <joshuavtk@hotmail.com>
2017-09-15 23:09:15 +0200

    fixed bug

--- Part_1/script.js
+++ Part_1/script.js
@@ -5,6 +5,30 @@ var secondVar, thirdVar, fourthVar, fifthVar, sixthVar, seventhVar,
     thirtheenthVar, fourteenthVar, fifteenthvar;
 
 //Variable assign & primitives
-secondVar = "a", thirdVar = 1, fourthVar = true, fifthVar = "bee", sixthVar = 2,
-    seventhVar = false, eigthVar = "c", ninthVar = 3, tenthVar = NaN,
-    eleventhVar = "d", twelfthVar = 4, thirtheenthVar = null, fourteenthVar = "e", fifteenthvar = 5;
+firstVar = "B";
+secondVar = "A";
+thirdVar = 1;
+fourthVar = true;
+fifthVar = "bee";
+sixthVar = 2;
+seventhVar = false;
+eigthVar = "c";
+ninthVar = 3;
+tenthVar = NaN;
+eleventhVar = "d";
+twelfthVar = 4.384937;
+thirtheenthVar = null;
+fourteenthVar = "e";
+fifteenthvar = 593746283658263867;
+
+//Reassign
+tenthVar = "f";
+thirtheenthVar = 6;
+
+console.log(firstVar, secondVar);
+
+var c = firstVar;
+firstVar = secondVar;
+secondVar = c;
+
+console.log(firstVar, secondVar);
