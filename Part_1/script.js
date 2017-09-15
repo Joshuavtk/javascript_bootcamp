ea727ce125108b6446fe9f2942395e3114f6b15e
Joshua <joshuavtk@hotmail.com>
2017-09-15 11:05:47 +0200

    Part 1
    Declared variables and made gitignore and created a index page

---
+++ Part_1/script.js
@@ -0,0 +1,5 @@
+//Variable Declare
+var firstVar;
+var secondVar, thirdVar, fourthVar, fifthVar, sixthVar, seventhVar,
+    eigthVar, ninthVar, tenthVar, eleventhVar, twelfthVar,
+    thirtheenthVar, fourteenthVar, fifteenthvar;
