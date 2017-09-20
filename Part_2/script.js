bf12908f37749af0551b2eee023b44caba47f574
Joshua <joshuavtk@hotmail.com>
2017-09-20 12:07:33 +0200

    created functions
    and logged some examples in the console

---
+++ Part_2/script.js
@@ -0,0 +1,12 @@
+//Functions
+function addWhitespace(text) {
+    return " " + text + " ";
+}
+
+console.log(addWhitespace("hoi"));
+
+function multiply(value, multiplier) {
+    return value * multiplier;
+}
+
+console.log(multiply(5, 10));
